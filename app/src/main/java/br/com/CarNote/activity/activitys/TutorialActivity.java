a0ea87ce2ef9799b496fe636a501364b10828367
Rodrigo Dias Soares <rodrigodiassoares19@gmail.com>
2021-02-21 11:00:53 -0300

    corrigindo mais detalhes do layout

---
+++ app/src/main/java/br/com/CarNote/activity/activitys/TutorialActivity.java
@@ -0,0 +1,16 @@
+package br.com.CarNote.activity.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.CarNote.R;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
